535ecfe4ee537e02bffc2b34265efcc3e836c0b9
Pavan Vadrevu <45509728+pavankumarsai18@users.noreply.github.com>
2021-04-27 12:26:37 -0400

    Updates the file to get the command line arguments

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// Create an empty file
+// Get the commandline arguments
+let commandInput = process.argv; 
+
+
+// get the numbers since the first two arguments are not required we slice them
+let numbers = commandInput.slice(2, commandInput.length); 
