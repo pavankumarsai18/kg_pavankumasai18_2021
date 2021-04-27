471f8622775fe954743388068b2693fd78278204
Pavan Vadrevu <45509728+pavankumarsai18@users.noreply.github.com>
2021-04-27 13:25:23 -0400

    Completes the program to handle negative numbers

--- main.js
+++ main.js
@@ -6,11 +6,32 @@ let commandInput = process.argv;
 // get the numbers since the first two arguments are not required we slice them
 let numbers = commandInput.slice(2, commandInput.length); 
 
-// Converts a number to phonetic equivalent
+// Converts a number to its phonetic equivalent
 function convertNumberToWord(num)
 {
-    let result = "";
+
+    let result = "";        // Holds the result
+    let isNegative = false; // A variable to determine whter the number is non-negative or negative
+
+    // Handles negative numbers
+    if(num < 0)
+    {
+        isNegative = true;
+        num *= -1;
+    }
+    else
+    {
+        // Handles the case when the number is -0
+        num = Math.abs(num);
+    }
+
+    // Array to hold the phonetic euivalents of all the digits
     let phoneticEquals = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+
+    if(num == 0)
+    {
+        result = phoneticEquals[num];
+    }
 
     // Gets each digit and updates the result string
     while(num > 0)
@@ -20,14 +41,18 @@ function convertNumberToWord(num)
         num = Math.floor(num/10);
     }
 
+    // If the number is negative we add Minus at the beginning of the result
+    if(isNegative == true)
+    {
+        result = "Minus" + result;
+    }
     return result;
 }
 
-// Converts all the numbers in an array to string
+// Converts all the numbers in an array into a string
 function convertArrayToWords(numbers)
 {
     let result = "";
-
     // Loop through the numbers
     for(let index = 0; index < numbers.length; index++)
     {
